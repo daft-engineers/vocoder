46e7d781be23497eb8f204c67e8bfb78ea197acf
Jambls <james.barlonachan@hotmail.co.uk>
2023-02-24 19:24:01 +0000

    added a check for files in the right place. made clang tidy error all the time

--- checks.sh
+++ checks.sh
@@ -3,22 +3,29 @@
 set -e
 
 INC=$(find include -name "*.hh")
-SRC=$(find src \( -name "*.cc" -o -name "*.hh" \))
+SRC=$(find src -name "*.cc")
 UNIT=$(find unit_tests -name "*.cc")
 WRONG_EXT=$(find src unit_tests \( -name "*.cpp" -o -name "*.hpp" \))
+WRONG_LOCATION=$(find src -name "*.hh" && find include -name "*.cc")
 
 if [[ $1 == "--static-analysis" ]]; then
-    clang-tidy -p build --warnings-as-errors='clang-diagnostic-*,clang-analyzer-*,clang-analyzer-*,concurrency-*,cppcoreguidelines-*,llvm-*,modernize-*,-modernize-use-trailing-return-type,performance-*' $INC $SRC 
+    clang-tidy -p build $INC $SRC 
 elif [[ $1 == "--formatting" ]]; then
     clang-format -n -Werror -style=file $INC $SRC $UNIT
     if [[ -n $WRONG_EXT ]]; then 
         echo "Files found ending in .cpp: $WRONG_EXT"
         exit 1
+    elif [[ -n $WRONG_LOCATION ]]; then 
+	echo "Header files found in src or cc files found in include"
+	exit 1
     fi
 else
     clang-tidy -p build $INC $SRC 
     clang-format -i -style=file $INC $SRC $UNIT
     if [[ -n $WRONG_EXT ]]; then
         echo "Files found ending in .cpp: $WRONG_EXT"
+    elif [[ -n $WRONG_LOCATION ]]; then 
+	echo "Header files found in src or cc files found in include"
+	exit 1
     fi
 fi
