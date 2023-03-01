c22cd154e06d7754e740dea0314038ebc3d36fde
MarkAHarley <markharley@hotmail.co.uk>
2023-03-01 20:12:50 +0000

    Fixed filter function logic and unit tests now work as expected (may need to account for IIR delay when moving on to higher frequencies)

--- unit_tests/filter/filter_test.cc
+++ unit_tests/filter/filter_test.cc
@@ -3,40 +3,38 @@
 #include <gtest/gtest.h>
 #include <vector>
 
+const unsigned int sample_size = 100;
+const double sampling_rate = 2000;
+const float threshold = 0.3;
+const float pi = 3.14;
+
 // Test Filter constructor
 TEST(FilterTest, Constructor) {
-    BPFilter f(2, 0, UINT_MAX);
-    // not sure what to test here yet
-    EXPECT_EQ(0, 0);
+BPFilter f(2, sampling_rate, 0, UINT_MAX);
+// not sure what to test here yet
+EXPECT_EQ(0, 0);
 }
-
-const unsigned int sample_size = 100;
-const float threshold = 0.3;
 
 // Test filter function is not returning all zeros
 TEST(FilterTest, EmptyReturn) {
     const unsigned int centre = 75;
     const unsigned int width = 20;
 
-    BPFilter f(10000, centre, width);
+    BPFilter f(2, sampling_rate, centre, width);
 
     // High freq sin
     const unsigned int freq = 75;
-    std::vector<float> sin_wav;
+    Audio sin_audio;
 
     for (int i = 0; i < sample_size; i++) {
-        sin_wav.push_back(sin(2 * i * M_PI * freq));
+        sin_audio.sample.push_back(sin(2 * i * pi * freq));
     }
-
-    Audio sin_audio;
-    sin_audio.sample = sin_wav;
 
     Audio out = f.filter(sin_audio);
 
     bool flag = false;
     for (int i = 0; i < sample_size; i++) {
         if (out.sample[i] > threshold) {
-            printf("%f", out.sample[i]);
             flag = true;
         }
     }
@@ -50,18 +48,15 @@ TEST(FilterTest, HighFrequencyResponse) {
     const unsigned int centre = 75;
     const unsigned int width = 20;
 
-    BPFilter f(10000, centre, width);
+    BPFilter f(2, sampling_rate, centre, width);
 
     // High freq sin
     const unsigned int high_freq = 200;
-    std::vector<float> high_sin;
+    Audio high_sin_audio;
 
     for (int i = 0; i < sample_size; i++) {
-        high_sin.push_back(sin(2 * i * M_PI * high_freq));
+        high_sin_audio.sample.push_back(sin(2 * i * pi * high_freq));
     }
-
-    Audio high_sin_audio;
-    high_sin_audio.sample = high_sin;
 
     Audio high_out = f.filter(high_sin_audio);
 
@@ -76,22 +71,19 @@ TEST(FilterTest, PassFrequencyResponse) {
     const unsigned int centre = 75;
     const unsigned int width = 20;
 
-    BPFilter f(10000, centre, width);
+    BPFilter f(2, sampling_rate, centre, width);
 
     // High freq sin
     const unsigned int pass_freq = 75;
-    std::vector<float> pass_sin;
+    Audio pass_sin_audio;
 
     for (int i = 0; i < sample_size; i++) {
-        pass_sin.push_back(sin(2 * i * M_PI * pass_freq));
+        pass_sin_audio.sample.push_back(sin(2 * i * pi * pass_freq));
     }
-
-    Audio pass_sin_audio;
-    pass_sin_audio.sample = pass_sin;
 
     Audio pass_out = f.filter(pass_sin_audio);
 
-    auto max = 0;
+    float max = 0;
     for (int i = 0; i < sample_size; i++) {
         if (pass_out.sample[i] > max) {
             max = pass_out.sample[i];
