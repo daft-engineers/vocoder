56449f587ea605c73fc964a01c595cc2d637810b
MarkAHarley <markharley@hotmail.co.uk>
2023-03-01 17:41:43 +0000

    Intial filter class and header

--- include/filter/filter.hh
+++ include/filter/filter.hh
@@ -1,4 +1,37 @@
-#ifndef FILTER_FILTER_HH
-#define FILTER_FILTER_HH
+#ifndef FILTER_H
+#define FILTER_H
 
-#endif // FILTER_FILTER_HH
+#include <Iir.h>
+#include "../audio/audio.hh"
+#include <limits.h>
+#include <stdio.h>
+
+class Filter
+{
+public:
+    Filter(unsigned int order_)
+            : order(order_){
+        if (order_ < 1){
+            printf("Invalid filter parameters.");
+            std::exit(-1);
+        }
+    }
+
+    virtual ~Filter() = default;
+
+    //placeholder
+    int setup(){
+        return order;
+    }
+
+    virtual Audio filter(Audio in_audio) = 0;
+
+private:
+    virtual void call_back() = 0;
+
+    //IIr::Butterworth::BandPass<order> f;
+    //int num_of_taps;
+    unsigned int order;
+};
+
+#endif
