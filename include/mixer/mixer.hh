a1aacdab4b4f0bb251502633d25377f8e10d5634
Matthew <2374138c@student.gla.ac.uk>
2023-03-17 19:39:30 +0000

    Add mixer prototype

--- include/mixer/mixer.hh
+++ include/mixer/mixer.hh
@@ -1,4 +1,24 @@
 #ifndef MIXER_MIXER_HH
 #define MIXER_MIXER_HH
 
+#include "../pipe.hh"
+#include "../audio/audio.hh"
+#include <array>
+#include <cstddef>
+#include <functional>
+
+namespace mixer {
+
+template <std::size_t num_banks> class Mixer {
+private:
+    std::array<Pipe<Audio>, num_banks> input_pipes;
+    Pipe<Audio> output_pipe;
+    
+public:
+    Mixer (std::array<Pipe<Audio>, num_banks> input_pipes, Pipe<Audio> output_pipe);
+    void run();
+};
+
+} // namespace mixer
+
 #endif // MIXER_MIXER_HH
