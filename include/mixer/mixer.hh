5116646d18dbfb68210765f4824bdce300279f68
Matthew <2374138c@student.gla.ac.uk>
2023-03-22 17:01:50 +0000

    Extract summation to new function and unit test

--- include/mixer/mixer.hh
+++ include/mixer/mixer.hh
@@ -3,23 +3,97 @@
 
 #include "../audio/audio.hh"
 #include "../pipe.hh"
+#include <algorithm>
 #include <array>
 #include <cstddef>
 #include <functional>
+#include <iostream>
+#include <memory>
+#include <mutex>
 
 namespace mixer {
 
+//  Mixer class takes in a list of input pipes, extracts the audio packet from
+//  each of them. It then produces a new audio packet which is the sum of the
+//  previous audio packets.
 template <std::size_t num_banks> class Mixer {
   private:
     std::array<Pipe<Audio>, num_banks> *input_pipes;
     Pipe<Audio> *output_pipe;
+
+    bool run_thread{};
+    std::thread thread;
 
   public:
     Mixer(std::array<Pipe<Audio>, num_banks> &input_pipes, Pipe<Audio> &output_pipe)
         : input_pipes(&input_pipes), output_pipe(&output_pipe) {
     }
 
+    ~Mixer() {
+        run_thread = false;
+        thread.join();
+    }
+
+    // explicitly disable all other contructors
+    Mixer(const Mixer &) = delete;
+    Mixer &operator=(const Mixer &) = delete;
+    Mixer(Mixer &&) = delete;
+    Mixer &operator=(Mixer &&) = delete;
+
+    static Audio sum(std::array<Audio, num_banks> audio_packets) {
+        Audio output_packet;
+
+        int num_samples = audio_packets[0].size();
+        output_packet.resize(num_samples);
+
+        for (int sample = 0; sample < num_samples; sample++) {
+            for (auto packet : audio_packets) {
+                output_packet[sample] += packet[sample];
+            }
+        }
+        return output_packet;
+    }
+
     void run() {
+        run_thread = true;
+        thread = std::thread([this]() {
+            while (run_thread) {
+                std::array<Audio, num_banks> audio_packets;
+
+                std::cerr << "run: starting run loop\n";
+
+                for (int bank = 0; bank < num_banks; bank++) {
+                    auto *pipe = &((*input_pipes)[bank]);
+
+                    std::unique_lock<std::mutex> lk(pipe->cond_m);
+                    pipe->cond.wait(lk, [&pipe] { return pipe->queue.empty(); });
+                    audio_packets[bank] = pipe->queue.front();
+                    pipe->queue.pop();
+                }
+
+                std::cerr << "run: extracted data from banks\n";
+                for (auto packet : audio_packets) {
+                    for (auto sample : packet) {
+                        std::cerr << sample << ", ";
+                    }
+                    std::cerr << "\n";
+                }
+
+                Audio output_packet = sum(audio_packets);
+                std::cerr << "run: summed values ";
+                for (auto x : output_packet)
+                    std::cerr << x << ", ";
+                std::cerr << "\n";
+
+                {
+                    std::lock_guard<std::mutex> lk(output_pipe->cond_m);
+                    output_pipe->queue.push(output_packet);
+                }
+                output_pipe->cond.notify_all();
+
+                std::cerr << "run: send value to output pipe\n";
+            }
+        });
     }
 };
 
--- unit_tests/mixer_tests.cc
+++ unit_tests/mixer_tests.cc
@@ -9,16 +9,28 @@
 
 // TEST macro violates guidelines
 // NOLINTNEXTLINE(cppcoreguidelines-owning-memory, cppcoreguidelines-avoid-non-const-global-variables)
+TEST(MixerTests, Summation) {
+    std::array<Audio, 2> inputs{{{1, 2, 3, 4, 5, 6}, {6, 5, 4, 3, 2, 1}}};
+    Audio expected{{7, 7, 7, 7, 7, 7}};
+    Audio result = mixer::Mixer<2>::sum(inputs);
+    ASSERT_EQ(result, expected);
+}
+
+// TEST macro violates guidelines
+// NOLINTNEXTLINE(cppcoreguidelines-owning-memory, cppcoreguidelines-avoid-non-const-global-variables)
 TEST(MixerTests, Initialise) {
     // set up mixer with 2 inputs
+    std::cerr << "setup: create vars\n";
     Pipe<Audio> output{};
     std::array<Pipe<Audio>, 2> inputs{};
     mixer::Mixer<2> mixer(inputs, output);
 
     // start mixer
+    std::cerr << "setup: start mixer\n";
     mixer.run();
 
     // provide input
+    std::cerr << "setup: create threads\n";
     std::thread input_thread1{[&inputs] {
         auto *pipe = &inputs[0];
         Audio sample{{10, 1, 34, 29, 98}};
@@ -28,6 +40,7 @@ TEST(MixerTests, Initialise) {
             pipe->queue.push(sample);
         }
         pipe->cond.notify_all();
+        std::cerr << "thread1: data pushed\n";
     }};
 
     std::thread input_thread2{[&inputs] {
@@ -39,19 +52,26 @@ TEST(MixerTests, Initialise) {
             pipe->queue.push(sample);
         }
         pipe->cond.notify_all();
+        std::cerr << "thread2: data pushed\n";
     }};
 
     // listen for output
     std::thread output_thread{[&output] {
         Audio expected{{33, 40, 117, 67, 126}};
         std::unique_lock<std::mutex> lk(output.cond_m);
-        output.cond.wait(lk, [&output] { return output.queue.empty() == false; });
-        ASSERT_EQ(output.queue.front().sample, expected.sample);
+        output.cond.wait(lk, [&output] {
+            std::cerr << "output thread: in output wait" << output.queue.size() << "\n";
+            return output.queue.empty() == false;
+        });
+        ASSERT_EQ(output.queue.front(), expected);
+        std::cerr << "output thread: assert ran\n";
     }};
 
-    output_thread.join();
-    input_thread2.join();
+    std::cerr << "setup: wait for threads to join\n";
     input_thread1.join();
+    input_thread2.join();
+    std::cerr << "setup: waiting for output thread only\n";
+    output_thread.join();
 }
 
 // NOLINTEND(cppcoreguidelines-avoid-magic-numbers)
