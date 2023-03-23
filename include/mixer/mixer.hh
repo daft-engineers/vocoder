ae20ca1da4a439a5b299eb586b93f0e841021f23
Matthew <2374138c@student.gla.ac.uk>
2023-03-23 12:34:57 +0000

    Change pointers to references

--- include/mixer/mixer.hh
+++ include/mixer/mixer.hh
@@ -17,15 +17,15 @@ namespace mixer {
 //  previous audio packets.
 template <std::size_t num_banks> class Mixer {
   private:
-    std::array<Pipe<Audio>, num_banks> *input_pipes;
-    Pipe<Audio> *output_pipe;
+    std::array<Pipe<Audio>, num_banks> &input_pipes;
+    Pipe<Audio> &output_pipe;
 
     bool run_thread{};
     std::thread thread;
 
   public:
     Mixer(std::array<Pipe<Audio>, num_banks> &input_pipes, Pipe<Audio> &output_pipe)
-        : input_pipes(&input_pipes), output_pipe(&output_pipe) {
+        : input_pipes(input_pipes), output_pipe(output_pipe) {
     }
 
     void stop() {
@@ -53,21 +53,21 @@ template <std::size_t num_banks> class Mixer {
             while (run_thread) {
                 std::array<Audio, num_banks> audio_packets;
                 for (int bank = 0; bank < num_banks; bank++) {
-                    auto *pipe = &((*input_pipes)[bank]);
+                    auto &pipe = input_pipes[bank];
 
-                    std::unique_lock<std::mutex> lk(pipe->cond_m);
-                    pipe->cond.wait(lk, [&pipe] { return pipe->queue.empty() == false; });
+                    std::unique_lock<std::mutex> lk(pipe.cond_m);
+                    pipe.cond.wait(lk, [&pipe] { return pipe.queue.empty() == false; });
 
-                    audio_packets[bank] = pipe->queue.front();
-                    pipe->queue.pop();
+                    audio_packets[bank] = pipe.queue.front();
+                    pipe.queue.pop();
                 }
 
                 Audio output_packet = sum(audio_packets);
                 {
-                    std::lock_guard<std::mutex> lk(output_pipe->cond_m);
-                    output_pipe->queue.push(output_packet);
+                    std::lock_guard<std::mutex> lk(output_pipe.cond_m);
+                    output_pipe.queue.push(output_packet);
                 }
-                output_pipe->cond.notify_all();
+                output_pipe.cond.notify_all();
             }
         });
     }
