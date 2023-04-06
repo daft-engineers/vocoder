9fe587ec697029251ec373cee59b69bfc02da3db
James Campbell <56744932+Jambls@users.noreply.github.com>
2023-04-06 14:49:19 +0100

    Added rms module (#76)
    * added rms module
    
    * fixed formatting
    
    * changed to use an int audio packet rather than uint
    
    * addressed comments and updated to the new wait_for hotness
    
    * added copy constructors and fixed ordering of includes
    
    * removed banned pointer

--- include/rms/rms.hh
+++ include/rms/rms.hh
@@ -1,4 +1,86 @@
+/* daft engineers vocoder project
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 #ifndef RMS_RMS_HH
 #define RMS_RMS_HH
+
+#include "../audio/audio.hh"
+#include "../pipe.hh"
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <numeric>
+
+namespace rms {
+
+template <std::size_t num_samples> class RMS {
+  private:
+    Pipe<Audio> &input_pipe;
+    Pipe<double> &output_pipe;
+
+    std::array<uint32_t, num_samples> squared_sample_buffer = {0};
+    int sample_buffer_index = 0;
+
+    const std::chrono::milliseconds timeout{1};
+    bool thread_alive{false};
+    std::thread thread;
+
+  public:
+    RMS(Pipe<Audio> &input_pipe, Pipe<double> &output_pipe) : input_pipe(input_pipe), output_pipe(output_pipe) {
+    }
+
+    void insert(Audio packet) {
+        std::for_each(packet.begin(), packet.end(), [this](const int16_t item) {
+            // this assumes that the 0 point for the signal is 0
+            uint32_t squared = item * item;
+            squared_sample_buffer[sample_buffer_index] = squared;
+            sample_buffer_index = (sample_buffer_index + 1) % num_samples;
+        });
+    }
+
+    double calc() {
+        uint64_t total = std::accumulate(std::begin(squared_sample_buffer), std::end(squared_sample_buffer), 0);
+        double output = std::sqrt(total / num_samples);
+        return output;
+    }
+
+    void run() {
+        thread = std::thread([this]() {
+            thread_alive = true;
+            while (true) {
+                {
+                    std::unique_lock<std::mutex> lk(input_pipe.cond_m);
+                    if (!input_pipe.cond.wait_for(lk, timeout, [this] { return input_pipe.queue.empty() == false; })) {
+                        return false;
+                    }
+
+                    insert(input_pipe.queue.front());
+                    input_pipe.queue.pop();
+                }
+
+                double output_packet = calc();
+                {
+                    std::lock_guard<std::mutex> lk(output_pipe.cond_m);
+                    output_pipe.queue.push(output_packet);
+                }
+                output_pipe.cond.notify_all();
+            }
+            return true;
+        });
+    }
+    ~RMS() {
+        if (thread_alive) {
+            thread.join();
+        }
+    }
+
+    RMS(const RMS &) = delete;
+    RMS &operator=(const RMS &) = delete;
+    RMS(RMS &&) = delete;
+    RMS &operator=(RMS &&) = delete;
+};
+} // namespace rms
 
 #endif // RMS_RMS_HH
---
+++ unit_tests/rms_tests.cc
@@ -0,0 +1,84 @@
+/* daft engineers vocoder project
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#include "../include/rms/rms.hh"
+#include <cmath>
+#include <gtest/gtest.h>
+#include <mutex>
+#include <thread>
+
+// This is a unit test, magic numbers are called test data here
+// NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers)
+
+// TEST macro violates guidelines
+// NOLINTNEXTLINE(cppcoreguidelines-owning-memory, cppcoreguidelines-avoid-non-const-global-variables)
+TEST(RMSTests, buffer) {
+    Pipe<Audio> input{};
+    Pipe<double> output{};
+    rms::RMS<4> rms(input, output);
+    Audio audio{0};
+
+    // check that rms is 0 at the beginning
+    ASSERT_EQ(rms.calc(), 0);
+
+    audio[0] = 16;
+    rms.insert(audio);
+    // buffer should now be 256, 0, 0, 0 giving rms of 8
+    ASSERT_EQ(rms.calc(), 8);
+
+    audio[0] = -16;
+    rms.insert(audio);
+    // buffer should now be 256, 256, 0, 0 giving rms of 8r2
+    ASSERT_EQ(rms.calc(), 8 * std::sqrt(2));
+
+    rms.insert(audio);
+    rms.insert(audio);
+    // buffer should now be 256, 256, 256, 256 giving rms of 16
+    ASSERT_EQ(rms.calc(), 16);
+
+    // insertions to the buffer from here should roll back to the front
+
+    audio[0] = 0;
+
+    for (int i = 0; i < 4; i++) {
+        rms.insert(audio);
+    }
+
+    // buffer should now be 0, 0, 0, 0, giving rms of 0
+    ASSERT_EQ(rms.calc(), 0);
+}
+
+// TEST macro violates guidelines
+// NOLINTNEXTLINE(cppcoreguidelines-owning-memory, cppcoreguidelines-avoid-non-const-global-variables)
+TEST(RMSTests, Integration) {
+    Pipe<Audio> input{};
+    Pipe<double> output{};
+    rms::RMS<4> rms(input, output);
+
+    rms.run();
+
+    std::thread input_thread{[&input] {
+        auto &pipe = input;
+        Audio sample{16, 0, 0, 0};
+
+        {
+            std::lock_guard<std::mutex> lk(pipe.cond_m);
+            pipe.queue.push(sample);
+        }
+        pipe.cond.notify_all();
+    }};
+
+    std::thread output_thread{[&output] {
+        std::unique_lock<std::mutex> lk(output.cond_m);
+        output.cond.wait(lk, [&output] { return output.queue.empty() == false; });
+        ASSERT_EQ(output.queue.front(), 8.0f);
+    }};
+
+    input_thread.join();
+    output_thread.join();
+}
+
+// NOLINTEND(cppcoreguidelines-avoid-magic-numbers)
