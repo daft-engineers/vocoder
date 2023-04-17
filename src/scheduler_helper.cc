de04ec61f7dc185f713b32e2c0f37130afa050b6
Angus Campbell <angus.barlonachan@hotmail.co.uk>
2023-04-17 16:27:05 +0100

    updated error message

--- src/scheduler_helper.cc
+++ src/scheduler_helper.cc
@@ -9,7 +9,7 @@ void Scheduler_helper::set_thread_priority(pthread_t handle, std::string const &
     sch.sched_priority = 20;
     if (pthread_setschedparam(handle, SCHED_FIFO, &sch)) {
         // NOLINTNEXTLINE(concurrency-mt-unsafe)
-        std::cerr << "Failed to set sched param (" << threadname << "):" << std::strerror(errno) << std::endl;
+        std::cerr << "Failed to set thread priority (" << threadname << "):" << std::strerror(errno) << std::endl;
         // note: thread should continue to run after without priority set.
         // Low priority should still work but may have a degraded experience.
     } else {
