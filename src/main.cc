da8d23ec612009c042608882acd3fdf98d9130cc
MHCooke <10051310+MHCooke@users.noreply.github.com>
2023-04-17 22:18:07 +0100

    Add command line arguments
    Closes #125

--- src/main.cc
+++ src/main.cc
@@ -6,7 +6,9 @@
 #include "../include/mixer.hh"
 #include "../include/pipe.hh"
 #include "../include/rms.hh"
+#include <cstdlib>
 #include <mutex>
+#include <ostream>
 
 // Doxygen mainpage
 /**
@@ -15,9 +17,40 @@
  * See navigation bar for classes and other documentation.
 */
 
-int main() {
+static void show_usage(const std::string &prog_name) {
+    std::cerr << "Usage: " << prog_name << " [-h] [-i input device] [-o output device]\n"
+              << "\nOptional arguments:\n"
+              << "    -i : The name of the ALSA capture device to use. Defaults to \"hw:2,0,0\"\n"
+              << "    -o : The name of the ALSA playback device to use. Defaults to \"hw:2,0,0\"\n"
+              << "    -h : Print this usage message\n";
+}
 
-    alsa_callback::acb alsa_in("hw:2,0,0");
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{};
+    args.reserve(argc);
+    for (int i = 0; i < argc; i++) {
+        args.emplace_back(argv[i]); // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic) it comes from C :(
+    }
+
+    std::string input_device_name = "hw:2,0,0";
+    std::string output_device_name = "hw:2,0,0";
+
+    auto help_flag = std::find(args.begin(), args.end(), "-h");
+    if (help_flag != args.end()) {
+        show_usage(args.at(0));
+    }
+
+    auto input_flag = std::find(args.begin(), args.end(), "-i");
+    if (input_flag != args.end()) {
+        input_device_name = *std::next(input_flag);
+    }
+
+    auto output_flag = std::find(args.begin(), args.end(), "-o");
+    if (output_flag != args.end()) {
+        output_device_name = *std::next(output_flag);
+    }
+
+    alsa_callback::acb alsa_in(input_device_name);
 
     const int num_filters = 10;
     const int num_samples = 300;
@@ -47,7 +80,7 @@ int main() {
     Pipe<Audio> mixer_out_pipe;
     mixer::Mixer<num_filters> mix(amp_out_pipes, mixer_out_pipe, timeout);
 
-    AlsaOut alsa_out("hw:2,0,0", mixer_out_pipe, timeout);
+    AlsaOut alsa_out(output_device_name, mixer_out_pipe, timeout);
     // Wire stuff together here
     for (int i = 0; i < num_filters; i++) {
         // Filters
