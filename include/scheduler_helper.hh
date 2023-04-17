32b828f2018fb2e3ef069796b0478538edfdc9b4
Angus Campbell <angus.barlonachan@hotmail.co.uk>
2023-04-17 14:14:07 +0100

    added documentation

--- include/scheduler_helper.hh
+++ include/scheduler_helper.hh
@@ -4,8 +4,18 @@
 #include <cstring>
 #include <iostream>
 #include <pthread.h>
+
+/**
+ * Scheduler helper Class. Holds the helper function
+ */
 class Scheduler_helper {
   public:
+    /**
+     * Helper function to set the thread priority. Avoids repeated code.
+     *
+     * @param handle Thread handle of the thread that needs its priority increased.
+     * @param threadname String to pass to debug outputs to see which thread failed
+     */
     static void set_thread_priority(pthread_t handle, std::string const &threadname);
 };
 
