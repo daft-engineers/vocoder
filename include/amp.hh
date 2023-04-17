74480565566dc52f901b550f2e2819923abaae96
MarkAHarley <markharley@hotmail.co.uk>
2023-04-17 17:25:46 +0100

    Add Amplifier Documentation

--- include/amp.hh
+++ include/amp.hh
@@ -4,8 +4,19 @@
 #include "audio.hh"
 #include "pipe.hh"
 
+/**
+ * Amplifier Class. This takes two input pipes, one containing Audio and one containing a double scaling value.
+ * This will output to a pipe containing Audio which is the input audio scaled by the scaling value.
+ */
 class Amplifier {
   public:
+    /**
+     * Constructs an Amplifier. Initialises the input Audio pipe, input scaling pipe, output Audio pipe and timeout.
+     * @param input_ Input Audio pipe.
+     * @param scaling_ Input Scaling pipe.
+     * @param output_ Output Audio pipe.
+     * @param timeout_ Time in milliseconds the thread will wait before exiting if no data provided.
+     */
     Amplifier(Pipe<Audio> &input_, Pipe<double> &scaling_, Pipe<Audio> &output_, std::chrono::milliseconds timeout_);
     ~Amplifier();
     Amplifier(const Amplifier &) = delete;
@@ -13,16 +24,30 @@ class Amplifier {
     Amplifier(Amplifier &&) = delete;
     Amplifier &operator=(Amplifier &&) = delete;
 
+    /**
+     * Run function. Opens a thread and continuously scales input audio by scaling, outputting the scaled audio.
+     */
     void run();
+    /**
+     * Amplify helper function. Scales a sample by scale value.
+     * @param sample The audio sample which is being scaled.
+     * @param amount The scale value to multiply the sample by.
+     */
     static Audio amplify(Audio &sample, double amount);
 
   private:
+    // Input Audio pipe.
     Pipe<Audio> &input;
+    // Input Scaling pipe.
     Pipe<double> &scaling;
+    // Output Audio pipe.
     Pipe<Audio> &output;
 
+    // Amplifier runtime thread.
     std::thread amplifier_thread;
+    // Boolean to determine if thread is alive for unit testing helper function.
     bool thread_alive{false};
+    // Timeout value for Amplifier thread.
     const std::chrono::milliseconds timeout;
 };
 
